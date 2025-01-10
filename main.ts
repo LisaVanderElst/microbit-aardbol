bbdff0a1e06cc5e220e9275cce99ff8a915647e1
LisaVanderElst <LisaVanderelst19@gmail.com>
2025-01-10 15:57:12 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,36 +1,36 @@
-basic.forever(function () {
-    let de_waarde_van_het_kompas = 0
-    if (de_waarde_van_het_kompas < 135 && de_waarde_van_het_kompas > 45) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else if (de_waarde_van_het_kompas < 225 && de_waarde_van_het_kompas > 315) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else if (de_waarde_van_het_kompas < 315 && de_waarde_van_het_kompas > 360) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else if (de_waarde_van_het_kompas < 135 && de_waarde_van_het_kompas > 225) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    }
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })
